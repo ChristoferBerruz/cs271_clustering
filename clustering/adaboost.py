5d2ac6882559ddf3d92489f88ed4102d12910b06
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2024-11-07 16:03:25 -0800

    Initial adaboost classifiers

---
+++ clustering/adaboost.py
@@ -0,0 +1,39 @@
+import numpy as np
+from typing import List, Callable
+from dataclasses import dataclass, field
+
+
+def adaboost(X: np.ndarray, z: np.ndarray, classifiers: List[Callable[[np.ndarray], int]]):
+    n_samples, m_features = X.shape
+    assert len(z) == n_samples, "Number of samples must match number of labels"
+    l_classifiers = len(classifiers)
+    coefficient_matrix = np.zeros((l_classifiers, n_samples))
+    update_c_matrix = coefficient_matrix
+    helper_coefficient = np.zeros((1, n_samples))
+    used = np.zeros(l_classifiers)
+    for m in range(l_classifiers):
+        if m == 0:
+            m_minus_one = 0
+            read_c_matrix = helper_coefficient
+        else:
+            m_minus_one = m - 1
+            read_c_matrix = coefficient_matrix
+        weights = np.zeros(n_samples)
+        for i in range(n_samples):
+            weights[i] = np.exp ** (-z[i]*read_c_matrix[m_minus_one, i])
+        W = np.sum(weights)
+        W2 = np.inf
+        t = None
+        for j in range(l_classifiers):
+            if used[j] == 0:
+                predictions = [classifiers[j](X[i]) for i in range(n_samples)]
+                Y = np.sum(np.where(predictions == z, weights))
+                if Y < W2:
+                    W2 = Y
+                    t = j
+        km = classifiers[t]
+        used[t] = 1
+        rm = W2 // W
+        alpha_m = 0.5*np.log((1 - rm)/rm)
+        new_val = read_c_matrix[m - 1, i] + alpha_m*km(X[i])
+        update_c_matrix[m, i] = new_val
